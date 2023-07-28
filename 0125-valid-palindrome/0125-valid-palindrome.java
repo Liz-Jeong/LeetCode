6f29224bb490ba2ff38aa1ce87c253f6e0ccbcfc
eli-jeong <70095017+Jung-gh@users.noreply.github.com>
2023-07-29 02:36:43 +0900

    Time: 128 ms (17.20%), Space: 44.9 MB (15.00%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        String fixed_string = "";
+        
+        // remove all non-alphanumeric characters
+        for(char c : s.toCharArray()) {
+            if(Character.isDigit(c) || Character.isLetter(c)) fixed_string += c;
+        }
+        
+        // to lowercase
+        fixed_string = fixed_string.toLowerCase();
+        
+        int a_pointer = 0;
+        int b_pointer = fixed_string.length() - 1;
+        
+        // compare the character from the front and end
+        while(a_pointer < b_pointer) {
+            if(fixed_string.charAt(a_pointer) != fixed_string.charAt(b_pointer)) return false;
+            a_pointer++;
+            b_pointer--;
+        }
+        
+        return true;
+    }
+}
