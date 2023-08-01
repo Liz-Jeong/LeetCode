02371161f072a37dc5e2a9dcc0519675ed75f2c8
eli-jeong <70095017+Jung-gh@users.noreply.github.com>
2023-08-01 20:03:00 +0900

    Time: 2 ms (95.93%), Space: 40.1 MB (99.42%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,9 +1,13 @@
 class Solution {
     
+    // Palindromic Substrings
+    // 1. if Palindromic Substrings or not
+    // 2. given string, find number of Palindromic Substrings
+    //     -> separate case of odd/even string length 
+    
     int count = 0;
     
     public int countSubstrings(String s) {
-        
         if(s == null || s.length() == 0) return 0;
         
         // Palindrom -> set the cases (odd, even)
@@ -17,7 +21,8 @@ class Solution {
     }
     
     public void isPelindrome(String s, int start, int end) {
-        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {    // check the pelindrome substring
+        // check the pelindrome substring
+        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
             count++;
             start--;    // index moved to left
             end++;      // index moved to right
