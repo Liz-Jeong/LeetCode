0d8ca8a7813907b17b12d036ff7cb6df7a1315fd
eli-jeong <70095017+Jung-gh@users.noreply.github.com>
2023-08-04 22:46:51 +0900

    Time: 14 ms (71.01%), Space: 47.4 MB (93.28%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        
+        // save each element's appearance in a table
         Map<Integer, Integer> map = new HashMap<>();
-        int[] result = new int[k];
-        
-        // put count into map
         for(int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
+        /* My Solution. keySet into List -> reverse sort -> save into Array for k
         List<Integer> keySet = new ArrayList<>(map.keySet());
+        
         // order by descending
         keySet.sort(new Comparator<Integer>() {
             @Override
@@ -19,9 +18,9 @@ class Solution {
         });
         
         int counter = 0;
+        int[] result = new int[k];
         
         for(Integer key : keySet) {
-        
             while(counter < k) {
                 result[counter] = keySet.get(counter);
                 counter++;
@@ -29,5 +28,23 @@ class Solution {
         }
         
         return result;
+        */
+        
+        // solution2. define min heap with size k
+        Queue<Integer> minHeap = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+        
+        //top k frequent element
+        for(int num : map.keySet()) {
+            minHeap.add(num);
+            if(minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        int[] res = new int[minHeap.size()];
+        int index = 0;
+        while(!minHeap.isEmpty()) {
+            res[index++] = minHeap.poll();
+        }
+        return res;
     }
 }
