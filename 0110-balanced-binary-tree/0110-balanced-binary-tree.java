c9d27569cb60bf0dbd2b16e48c41a93be013cf0b
eli-jeong <70095017+Jung-gh@users.noreply.github.com>
2023-08-09 14:20:30 +0900

    Time: 0 ms (100.00%), Space: 43.1 MB (69.59%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,31 +14,45 @@
  * }
  */
 class Solution {
-    boolean res = true;
+    // 1. have to traverse all the nodes
+    //boolean res = true;
+    
+    // 2. return false right away
+    final int UNBALANCED = -1;
+    
     public boolean isBalanced(TreeNode root) {
+        // Balanced Tree : height difference between left and right of every node no bigger than 1.
+        
         // base case
         if(root == null) return true;
+        return dfs(root) != UNBALANCED;
         
-        // recursive
-        dfs(root);
-        return res;
+        //return res;
     }
     
     private int dfs(TreeNode root) {
         
         int left = 0, right = 0;
         
+        // Left Subtree DFS
         if(root.left != null) {
             left = dfs(root.left);
         }
+        if(left == UNBALANCED) return UNBALANCED;
+        
+        // Right Subtree DFS
         if(root.right != null) {
             right = dfs(root.right);
         }
-        // check if it is not height-balanced tree.
-        if(Math.abs(left - right) > 1) {
-            res = false;
-        }
-        return Math.max(left, right) + 1;
+        if(right == UNBALANCED) return UNBALANCED;
         
+        // check if it is not height-balanced tree.
+        // if(Math.abs(left - right) > 1) {
+        //     res = false;
+        // }
+        // return Math.max(left, right) + 1;
+        
+        if(Math.abs(left - right) <= 1) return Math.max(left, right) + 1;
+        return UNBALANCED;
     }
 }
