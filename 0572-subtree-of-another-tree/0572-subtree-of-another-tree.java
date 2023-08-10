066f74e4be8e38797a6bdd525a4919489cabdeb6
eli-jeong <70095017+Jung-gh@users.noreply.github.com>
2023-08-10 19:15:47 +0900

    Time: 2 ms (98.52%), Space: 43.3 MB (61.03%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,13 +15,15 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        // base case
-        if(root == null || subRoot == null) return false;
+        // base case - check if both root and subRoot exist
+        if(subRoot == null) return true;
+        if(root == null) return false;
         
         // check if the subtrees are same
-        if(root.val == subRoot.val && DFS(root, subRoot)) {
+        if(DFS(root, subRoot)) {
             return true;
         }
+        
         // find root which is same as subRoot
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
@@ -29,8 +31,10 @@ class Solution {
     public boolean DFS(TreeNode tree, TreeNode subtree) {
         if(tree == null && subtree == null) return true;
         if(tree == null || subtree == null) return false;
+        
         // compare two nodes
         if(tree.val != subtree.val) return false;
+        
         // if the values are same, continue to traverse nodes in the next level
         return DFS(tree.left, subtree.left) && DFS(tree.right, subtree.right); 
     }
