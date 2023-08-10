567a9a78f992d2fe09d293ea54374c240f620acc
eli-jeong <70095017+Jung-gh@users.noreply.github.com>
2023-08-11 01:44:08 +0900

    Time: 5 ms (98.49%), Space: 43.7 MB (95.47%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -13,11 +13,20 @@ class Solution {
         
         // binary search tree - O(log n), O(1)
         
+        // 1. recursive
         // p == root || q == root => LCA : root
-        if(root.val == p.val || root.val == q.val) return root;
-        if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
-        if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        // if(root.val == p.val || root.val == q.val) return root;
+        // if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        // if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        // return root;
         
+        // 2. O(1) space complexity
+        while(root != null) {
+            int cur = root.val;
+            if(cur > p.val && cur > q.val) root = root.left;
+            else if(cur < p.val && cur < q.val) root = root.right;
+            else break;
+        }
         return root;
     }
 }
