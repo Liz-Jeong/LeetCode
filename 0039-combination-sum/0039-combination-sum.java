ceccdd7008a65da4f2f3cc575645a29242eb5baa
Liz Jeong <70095017+Liz-Jeong@users.noreply.github.com>
2023-08-18 17:51:08 +0900

    Time: 3 ms (36.29%), Space: 43.3 MB (99.49%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,35 +2,31 @@ class Solution {
     
     List<List<Integer>> res = new LinkedList<>();
     int[] nums;
+    int target;
     
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         
         nums = candidates;
-        DFS(new LinkedList<>(), target, 0);
+        this.target = target;
+        
+        DFS(new LinkedList<>(), 0, 0);
         return res;
     }
     
-    private void DFS(List<Integer> sub, int target, int curIdx) {
+    private void DFS(List<Integer> sub, int sum, int curIdx) {
+        
+        // using 'sum' instead of 'target' to enhance readability
+        if(sum > target) return;
+        if(sum == target) {
+            res.add(new LinkedList<>(sub));
+            return;
+        }
         
         // using recurrent tree
         for(int i = curIdx; i < nums.length; i++) {
-            
-            // return sub : target - val = 0
-            if(target - nums[i] == 0) {
-                sub.add(nums[i]);
-                res.add(new LinkedList<>(sub));
-                sub.remove(sub.size() - 1);
-            }
-            
-            // keep traversing : target - val > 0
-            if(target - nums[i] > 0) {
-                sub.add(nums[i]);
-                DFS(sub, target - nums[i], i);
-                sub.remove(sub.size() - 1);
-            }
-            
-            curIdx++;
-            
+            sub.add(nums[i]);
+            DFS(sub, sum + nums[i], i);
+            sub.remove(sub.size() - 1);
         }
         
         
