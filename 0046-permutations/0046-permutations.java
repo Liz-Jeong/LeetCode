10b76b38d8c377f6d586eb2611cec5db5cb77120
Liz Jeong <70095017+Liz-Jeong@users.noreply.github.com>
2023-08-19 17:01:09 +0900

    Time: 3 ms (8.19%), Space: 44.1 MB (38.57%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -4,34 +4,47 @@ class Solution {
     int[] arr;
     
     public List<List<Integer>> permute(int[] nums) {
-        arr = nums;    
-        DFS(new LinkedList<>());
+        
+        // 2. the other way to solve this problem : use List<Integer> decision tree
+        List<Integer> dt = new LinkedList<>();
+        for(int num : nums) {
+            dt.add(num);    
+        }
+        DFS(new LinkedList<>(), dt);
         return res;
+        
+        // 1. DFS
+        // arr = nums;    
+        // DFS(new LinkedList<>());
+        // return res;
     }
     
-    private void DFS(List<Integer> subset) {
+    private void DFS(List<Integer> permutation, List<Integer> dt) {
         
-        if(subset.size() == arr.length) {
-            res.add(new ArrayList<>(subset));
+        if(dt.size() == 0) {
+            res.add(permutation);
             return;
         }
-
-        for(int i = 0; i < arr.length; i++) {
-            if(subset.contains(arr[i])) continue;   // duplicated value is not acceptable
-            subset.add(arr[i]);
-            DFS(subset);
-            subset.remove(subset.size() - 1);
+        
+        for(Integer num : dt) {
+            List<Integer> tempP = new LinkedList<>(permutation);
+            List<Integer> tempDt = new LinkedList<>(dt);
+            tempP.add(num);     // [1]
+            tempDt.remove(num);
+            DFS(tempP, tempDt); // [2, 3]
         }
         
-        // [1,2,3]
-        
-        // [1]
-        // [1, 2] [1, 3]
-        // [1, 2, 3] [1, 3, 2]
-        
-        // [2]
-        // [2, 1] [2, 3]
-        // [2, 1, 3] [2, 3, 1]
+//         if(permutation.size() == arr.length) {
+//             res.add(new ArrayList<>(permutation));
+//             return;
+//         }
+
+//         for(int i = 0; i < arr.length; i++) {
+//             if(permutation.contains(arr[i])) continue;   // duplicated value is not acceptable
+//             permutation.add(arr[i]);
+//             DFS(permutation);
+//             permutation.remove(permutation.size() - 1);
+//         }
         
     }
 }
