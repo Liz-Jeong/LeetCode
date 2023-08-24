8d62ecc2ffff5c39d3cacb92e7c955a430918609
Liz Jeong <70095017+Liz-Jeong@users.noreply.github.com>
2023-08-24 22:59:06 +0900

    Time: 1 ms (78.69%), Space: 41 MB (87.65%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,43 +1,60 @@
 class Solution {
     
-    HashMap<Character, String> phone = new HashMap<>();
+    HashMap<Character, String> phone = new HashMap<>() {{
+        put('2', "abc");
+        put('3', "def");
+        put('4', "ghi");
+        put('5', "jkl");
+        put('6', "mno");
+        put('7', "pqrs");
+        put('8', "tuv");
+        put('9', "wxyz");
+    }};
     
     List<String> res = new ArrayList<>();
     char[] input;
     
     public List<String> letterCombinations(String digits) {
         
-        phone.put('2', "abc");
-        phone.put('3', "def");
-        phone.put('4', "ghi");
-        phone.put('5', "jkl");
-        phone.put('6', "mno");
-        phone.put('7', "pqrs");
-        phone.put('8', "tuv");
-        phone.put('9', "wxyz");
+        if(digits.length() == 0) return res;
+        this.input = digits.toCharArray();
         
-        input = digits.toCharArray();
-        if(input.length == 0) return res;
-        
-        DFS("", 0);
+        // make improvement using StringBuilder
+        DFS(new StringBuilder(), 0);
+        //DFS("", 0);
         return res;
     }
     
-    private void DFS(String str, int index) {
-        
-        if(str.length() == input.length) {
-            res.add(new String(str));
+    private void DFS(StringBuilder sb, int index) {
+        //Base Case
+        if(index == input.length) {
+            res.add(sb.toString());
             return;
         }
         
         String nums = phone.get(input[index]);
-        char[] list = nums.toCharArray();
-        int strLen = phone.get(input[index]).length(); //abc
-        
-        for(int i = 0; i < strLen; i++) {
-            str += list[i];
-            DFS(str, index + 1);
-            str = str.substring(0, str.length() - 1);
+        for(char cur : nums.toCharArray()) {
+            sb.append(cur);
+            DFS(sb, index + 1);
+            sb.setLength(sb.length() - 1);
         }
     }
+    
+//     private void DFS(String str, int index) {
+        
+//         if(str.length() == input.length) {
+//             res.add(new String(str));
+//             return;
+//         }
+        
+//         String nums = phone.get(input[index]);
+//         char[] list = nums.toCharArray();
+//         int strLen = phone.get(input[index]).length(); //abc
+        
+//         for(int i = 0; i < strLen; i++) {
+//             str += list[i];
+//             DFS(str, index + 1);
+//             str = str.substring(0, str.length() - 1);
+//         }
+//     }
 }
