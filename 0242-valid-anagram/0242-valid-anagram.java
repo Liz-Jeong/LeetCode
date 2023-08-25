2f5eca73252fff101ed6ed17e3e2ac1bb05720ff
Liz Jeong <70095017+Liz-Jeong@users.noreply.github.com>
2023-08-25 13:42:26 +0900

    Time: 12 ms (34.67%), Space: 43.4 MB (59.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,35 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        char[] first = s.toCharArray();
-        char[] second = t.toCharArray();
+        // 1. Sorting -> time complexity of O(n log n) due to the sorting operation
+        // char[] first = s.toCharArray();
+        // char[] second = t.toCharArray();
         
-        Arrays.sort(first);
-        Arrays.sort(second);
+        // Arrays.sort(first);
+        // Arrays.sort(second);
         
-        if(Arrays.equals(first, second)) {
-            return true;
-        }else {
-            return false;
+        // if(Arrays.equals(first, second)) {
+        //     return true;
+        // }else {
+        //     return false;
+        // }
+        
+        //2. Hash Table -> time complexity of O(n)
+        Map<Character, Integer> count = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
         }
-        
+
+        for (char c : t.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) - 1);
+        }
+
+        // check if all characters have zero frequency
+        for(int val : count.values()) {
+            if(val != 0) return false;
+        }
+
+        return true;
     }
 }
