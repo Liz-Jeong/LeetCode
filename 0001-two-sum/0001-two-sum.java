43be677a6c17971a07c6e5af53821a6cae33ae4b
Liz Jeong <70095017+Liz-Jeong@users.noreply.github.com>
2023-08-26 14:30:47 +0900

    Time: 5 ms (60.98%), Space: 43.2 MB (98.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,34 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
+        // Time complexity : O(n)
         Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
         
-        int arr[] = new int[2];
-        int n = nums.length;
-            
-        // for(int i = 0; i < nums.length; i++){
-        //     for(int j = 0; j < nums.length; j++){
-        //         if(i == j) continue;
-        //         if(nums[i] + nums[j] == target){
-        //             arr[0] = i;
-        //             arr[1] = j;
-        //             return arr;
-        //         } 
-        //     }    
-        // }
-        
-        // improvement
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < nums.length; i++) {
             
             if(map.containsKey(target - nums[i])) {
-                //return index from nums
-                arr[0] = map.get(target - nums[i]);
-                arr[1] = i;
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
             }
+            
+            // add value, index
             map.put(nums[i], i);
+            
         }
-        return arr;
+        
+        return res;
     }
 }
