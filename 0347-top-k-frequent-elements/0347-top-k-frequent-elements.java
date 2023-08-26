72b2050266e88e60532036d8d4aef08c620ab3f1
Liz Jeong <70095017+Liz-Jeong@users.noreply.github.com>
2023-08-26 14:58:25 +0900

    Time: 14 ms (61.63%), Space: 47.6 MB (68.78%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,50 +1,27 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // save each element's appearance in a table
+        
         Map<Integer, Integer> map = new HashMap<>();
-        for(int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        int[] res = new int[k];
+        
+        // save each element appearance in a table
+        for(int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
         
-        /* My Solution. keySet into List -> reverse sort -> save into Array for k
-        List<Integer> keySet = new ArrayList<>(map.keySet());
+        // MinHeap with size k
+        Queue<Integer> minHeap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
         
-        // order by descending
-        keySet.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer a, Integer b) {
-                return map.get(b).compareTo(map.get(a));
-            }
-        });
-        
-        int counter = 0;
-        int[] result = new int[k];
-        
-        for(Integer key : keySet) {
-            while(counter < k) {
-                result[counter] = keySet.get(counter);
-                counter++;
-            }
+        // add key into the minHeap, but ordered by value
+        for(int key : map.keySet()) {    // 1:3, 2:2, 3:1
+            minHeap.add(key);
         }
         
-        return result;
-        */
-        
-        // solution2. define min heap with size k
-        Queue<Integer> minHeap = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
-        
-        //top k frequent element
-        for(int num : map.keySet()) {
-            minHeap.add(num);
-            if(minHeap.size() > k) {
-                minHeap.poll();
-            }
-        }
-        int[] res = new int[minHeap.size()];
         int index = 0;
-        while(!minHeap.isEmpty()) {
+        for(int i = 0; i < k; i++) {
             res[index++] = minHeap.poll();
         }
+        
         return res;
     }
 }
